89aecd0255886d1cae5edd178844ec6d0d6da1a7
nagasubbaraosatyamDevops <nagasubbaraosatyam@gmail.com>
2026-05-04 16:15:28 +0530

    terraform data

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-resource "vault_mount" "component" {
-  path     = "roboshop-dev"
+resource "vault_mount" "secret-mounts" {
+  for_each = var.secret-mounts
+  path     = each.key
   type     = "kv-v2"
   options = {
     version = "2"
     type    = "kv-v2"
   }
-  description = "Roboshop Dev"
+  description = each.value["description"]
 }
--- variable.tf
+++ variable.tf
@@ -1,1 +1,9 @@
 variable "token" {}
+
+variable "secret-mounts" {
+  default={
+    roboshop-dev={
+      description="roboshop Project Dev Secrets"
+    }
+  }
+}
